2afca2742f6992500f66fc485d11895448d9a496
whoami-789 <74957458+whoami-789@users.noreply.github.com>
2022-04-12 10:21:25 +0300

    Initial commit

---
+++ src/main/java/com/example/gm/GmApplication.java
@@ -0,0 +1,13 @@
+package com.example.gm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gm/GmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
