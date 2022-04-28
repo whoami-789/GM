9ce4d97b8840da0db54b12b4724ac656f65854f3
whoami-789 <74957458+whoami-789@users.noreply.github.com>
2022-04-28 23:09:37 +0300

    git commit

--- src/main/java/com/example/gm/models/Users.java
+++ src/main/java/com/example/gm/models/Users.java
@@ -1,4 +1,31 @@
 package com.example.gm.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "bdate")
+    private String bdate;
+    @Column(name = "phoneNumber")
+    private String phoneNumber;
+    @Column(name = "login")
+    private String login;
+    @Column(name = "pass")
+    private String password;
 }
